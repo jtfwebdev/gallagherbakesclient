6a6a6440cd6578675a6ce65aebbb8a6eb996f432
Josh Ford <jtfwebdevconsultant@hotmail.com>
2024-05-20 13:52:25 +0100

    added responsive styling to shop page

--- src/assets/Components/Shop.tsx
+++ src/assets/Components/Shop.tsx
@@ -1,5 +1,3 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCartPlus } from '@fortawesome/free-solid-svg-icons';
 import { useNavigate } from 'react-router-dom';
 import { motion } from 'framer-motion';
 
@@ -35,22 +33,26 @@ const Home = ({products}: {products: any[] | null}) => {
     }
 
     return ( 
-        <div className="w-[80%] mx-auto h-fit mt-16">
+        <div className="
+        w-[80%] mx-auto h-fit mt-16
+        max-[1100px]:w-[95%]
+        max-[425px]:w-[98%]
+        ">
             <h2 className="font-text text-secondary-100 text-2xl mb-4 border-b-2 border-secondary-100 w-fit">Shop at Gallagher Bakes</h2>
             <div className="
             h-fit w-full grid grid-cols-4
+            max-[800px]:grid-cols-2
             ">
                 {products && products.map((product, idx) => {
                     return(
-                        <motion.div key={idx} className="p-4 rounded flex flex-col hover:cursor-pointer" initial="initial" whileHover="hover" onClick={() => handleClick(product.slug)}>
+                        <motion.div key={idx} className="p-1 py-4 rounded flex flex-col hover:cursor-pointer" initial="initial" whileHover="hover" onClick={() => handleClick(product.slug)}>
                             <div className="overflow-hidden">
                                 <motion.img className="rounded" variants={imgVars} src={product.images[0].src} alt="" />
                             </div>
-                            <motion.p className="text-secondary-100 font-text flex-1 flex font-bold items-start" variants={textVars}>{product.name}</motion.p>
+                            <motion.p className="text-secondary-100 font-text flex-1 flex font-bold text-xl items-start" variants={textVars}>{product.name}</motion.p>
                             <div className="text-secondary-100 font-text text-sm mt-2" dangerouslySetInnerHTML={{__html: product.description}}></div>
                             <div className="mt-2 flex items-center justify-between">
                                 <div className="text-secondary-100 font-text inline">£{product.price}</div>
-                                <FontAwesomeIcon className="text-secondary-100 text-lg p-2 hover:cursor-pointer hover:opacity-80 duration-150" icon={faCartPlus} />
                             </div>
                         </motion.div>
                     )
